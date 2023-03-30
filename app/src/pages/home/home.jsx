cc618d3c8eb2ccbf03bde16e5650f888ee67ec9b
Tawanda Moyo <moyotawanda@gmail.com>
2023-03-30 11:22:55 +0200

    Add home page

---
+++ app/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <h2>Hello, welcome to Nota</h2>
+            <button>Upload My Clippings File</button>
+        </>
+    )
+}
+
+export default Home;
